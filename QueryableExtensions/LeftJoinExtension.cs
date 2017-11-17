29e14e01e9566a5369172ccfac601dfd1669a202
Sudarsha Hewa <sudarsha.hewa@canada.ca>
2017-11-17 13:29:23 -0500

    LeftJoin extension implemented using LinkqKit.Core library.

--- QueryableExtensions/LeftJoinExtension.cs
+++ QueryableExtensions/LeftJoinExtension.cs
@@ -1,10 +1,52 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using LinqKit;
 
 namespace QueryableExtensions
 {
+    public static class JoinExtensions
+    {
+        /// <summary>
+        /// Implement Left Outer join implemented by calling GroupJoin and
+        /// SelectMany within this extension method
+        /// </summary>
+        /// <typeparam name="TOuter">Outer Type</typeparam>
+        /// <typeparam name="TInner">Inner Type</typeparam>
+        /// <typeparam name="TKey">Key Type</typeparam>
+        /// <typeparam name="TResult">Result Type</typeparam>
+        /// <param name="outer">Outer set</param>
+        /// <param name="inner">Inner set</param>
+        /// <param name="outerKeySelector">Outer Key Selector</param>
+        /// <param name="innerKeySelector">Inner Key Selector</param>
+        /// <param name="resultSelector">Result Selector</param>
+        /// <returns>IQueryable Result set</returns>
+        public static IQueryable<TResult> LeftJoin<TOuter, TInner, TKey, TResult>(
+           this IQueryable<TOuter> outer,
+           IQueryable<TInner> inner,
+           Expression<Func<TOuter, TKey>> outerKeySelector,
+           Expression<Func<TInner, TKey>> innerKeySelector,
+           Expression<Func<TOuter, TInner, TResult>> resultSelector)
+        {
+            //LinqKit allows easy runtime evaluation of inline invoked expressions
+            // without manually writing expression trees.
+            return outer
+                .AsExpandable()// Tell LinqKit to convert everything into an expression tree.
+                .GroupJoin(
+                    inner,
+                    outerKeySelector,
+                    innerKeySelector,
+                    (outerItem, innerItems) => new { outerItem, innerItems })
+                .SelectMany(
+                    joinResult => joinResult.innerItems.DefaultIfEmpty(),
+                    (joinResult, innerItem) =>
+                        resultSelector.Invoke(joinResult.outerItem, innerItem));
+        }
+    }
+
+    /**************************************************************************
+     * No luck with expression based solution.
+     *
 
     // Much of the code copied from following URL:
     // https://stackoverflow.com/questions/21615693/extension-method-for-iqueryable-left-outer-join-using-linq
@@ -164,4 +206,5 @@ namespace QueryableExtensions
             return (IQueryable<TResult>)selectMany1Result;
         }
     }
+    **************************************************************************/
 }
